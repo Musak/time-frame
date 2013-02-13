19d1a47bbd87826e82dac2a07af0a5e33c9caf49
Ionut <ionut.iancau@russmedia.com>
2013-02-13 15:49:37 +0200

    Added validation for unique days

--- src/RMT/TimeScheduling/DayIntervalValidator.php
+++ src/RMT/TimeScheduling/DayIntervalValidator.php
@@ -4,6 +4,7 @@ namespace RMT\TimeScheduling;
 
 use Symfony\Component\Validator\ExecutionContext;
 use RMT\TimeScheduling\Model\DayInterval;
+use RMT\TimeScheduling\Model\DayIntervalQuery;
 
 class DayIntervalValidator
 {
@@ -12,6 +13,18 @@ class DayIntervalValidator
         if($day_interval->getStartHour() >= $day_interval->getEndHour())
         {
        	    $context->addViolationAtSubPath('start_hour', 'The start must be earlier than the end hour.', array(), null);
+        }
+    }
+    public static function isUniqueDay(DayInterval $day_interval, ExecutionContext $context)
+    {
+    	$count = DayIntervalQuery::create()
+    		->filterByUser($day_interval->getUser())
+    		->filterByDay($day_interval->getDay())
+    		->count();
+
+        if($count > 0)
+        {
+       	    $context->addViolationAtSubPath('day', 'You already defined hours for this day.', array(), null);
         }
     }
 }
