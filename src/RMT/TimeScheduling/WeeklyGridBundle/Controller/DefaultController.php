ccc3dc9924b1451cb97298e3bffb41538c00bfe2
Daniel Andrei Adrian <adrin3l@yahoo.com>
2013-02-14 17:02:10 +0200

    Add weekly table grid with working hours WIP

--- src/RMT/TimeScheduling/WeeklyGridBundle/Controller/DefaultController.php
+++ src/RMT/TimeScheduling/WeeklyGridBundle/Controller/DefaultController.php
@@ -3,13 +3,26 @@
 namespace RMT\TimeScheduling\WeeklyGridBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use RMT\TimeScheduling\Model\DayIntervalQuery;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
 
+    	$user = $this->getUser();
+        $day_intervals = DayIntervalQuery::create()
+            ->filterByUser($user)
+            ->find();
+        
 
-        return $this->render('RMTTimeSchedulingWeeklyGridBundle:Default:index.html.twig');
+        foreach($day_intervals as $day_interval){
+
+    	$days[$day_interval->getDay()->getValue()] = array(
+				'start_hour' => $day_interval->getStartHour()->format('H'),
+				'end_hour' => $day_interval->getEndHour()->format('H')
+			);
+        }
+        return $this->render('RMTTimeSchedulingWeeklyGridBundle:Default:index.html.twig',array('days'=>$days));
     }
 }
