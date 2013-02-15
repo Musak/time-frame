b68241a90e6b376621491a85825d4ded420f511f
Crina Stranici <crina.stranici@russmedia.com>
2013-02-15 11:34:57 +0200

    display colored working hours in weekly grid

--- src/RMT/TimeScheduling/WeeklyGridBundle/Controller/DefaultController.php
+++ src/RMT/TimeScheduling/WeeklyGridBundle/Controller/DefaultController.php
@@ -15,14 +15,15 @@ class DefaultController extends Controller
             ->filterByUser($user)
             ->find();
         
-
+        $days = array();
         foreach($day_intervals as $day_interval){
 
-    	$days[$day_interval->getDay()->getValue()] = array(
-				'start_hour' => $day_interval->getStartHour()->format('H'),
-				'end_hour' => $day_interval->getEndHour()->format('H')
-			);
+        	$days[$day_interval->getDay()->getValue()] = array(
+    				'start_hour' => $day_interval->getStartHour()->format('H'),
+    				'end_hour' => $day_interval->getEndHour()->format('H')
+    			);
         }
+        //var_dump($days);exit;
         return $this->render('RMTTimeSchedulingWeeklyGridBundle:Default:index.html.twig',array('days'=>$days));
     }
 }
