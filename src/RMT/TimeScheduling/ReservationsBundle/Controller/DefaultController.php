a5282b32c64d1c0179998648a10d87779a9fd746
Elena Alexie <elena.coman@russmedia.com>
2013-02-18 16:41:31 +0200

    Work in progress, reservation entry action.

--- src/RMT/TimeScheduling/ReservationsBundle/Controller/DefaultController.php
+++ src/RMT/TimeScheduling/ReservationsBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace RMT\TimeScheduling\ReservationsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use RMT\TimeScheduling\Model\Reservation;
+use RMT\TimeScheduling\ReservationsBundle\Form\Type\ReservationType;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('RMTTimeSchedulingReservationsBundle:Default:index.html.twig', array('name' => $name));
+    	$request = $this->getRequest();
+    	$reservation = new Reservation();
+    	$form = $this->createForm(new ReservationType(), $reservation);
+    	
+    	if($request->getMethod() == 'POST')
+    	{
+    		$form->bindRequest($request);
+            if ($form->isValid()) {
+            	$reservation->save();
+            }
+    	}
+        return $this->render('RMTTimeSchedulingReservationsBundle:Default:index.html.twig', array('form' => $form->createView()));
     }
 }
---
+++ src/RMT/TimeScheduling/ReservationsBundle/Form/Type/ReservationType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RMT\TimeScheduling\ReservationsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('reserver_user_id', 'model', array(
+            'class' => 'FOS\UserBundle\Propel\User',
+        ));
+        $builder->add('reservee_user_id', 'model', array(
+            'class' => 'FOS\UserBundle\Propel\User',
+        ));
+        $builder->add('day', 'model', array(
+            'class' => 'RMT\TimeScheduling\Model\Day',
+        ));
+        $builder->add('start_time');
+        $builder->add('end_time');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RMT\TimeScheduling\Model\Reservation',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'reservation';
+    }
+}
