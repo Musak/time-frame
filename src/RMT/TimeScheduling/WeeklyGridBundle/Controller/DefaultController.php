0524d7c09b07959c4d43f03e3368263c4d768b53
Ionut <ionut.iancau@russmedia.com>
2013-02-21 16:12:19 +0200

    Work in progress

--- src/RMT/TimeScheduling/WeeklyGridBundle/Controller/DefaultController.php
+++ src/RMT/TimeScheduling/WeeklyGridBundle/Controller/DefaultController.php
@@ -4,12 +4,16 @@ namespace RMT\TimeScheduling\WeeklyGridBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use RMT\TimeScheduling\Model\DayIntervalQuery;
+use RMT\TimeScheduling\Model\ReservationQuery;
+use FOS\UserBundle\Propel\UserQuery;
 
 class DefaultController extends Controller
 {
     public function indexAction($user_id)
     {
     	$user = $this->getUser();
+        $service_provider_user = UserQuery::create()
+            ->findPK($user_id);
         $day_intervals = DayIntervalQuery::create()
             ->filterByUserId($user_id)
             ->find();
@@ -24,11 +28,17 @@ class DefaultController extends Controller
     				'end_hour' => $day_interval->getEndHour()->format('H')
     			);
         }
+        $reserved = ReservationQuery::create()
+            ->filterByServiceProvider($service_provider_user)
+            ->find()
+            ->toKeyValue('StartTime','EndTime');
 
         return $this->render('RMTTimeSchedulingWeeklyGridBundle:Default:index.html.twig',array(
           'days'                => $days,
           'my_grid'             => $my_grid,
+          'reserved'            => $reserved,
           'service_provider_id' => $user_id
+
         ));
     }
 }
